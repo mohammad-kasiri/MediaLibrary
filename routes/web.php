a52e2383a485325ee8d1f7864f6a0c11dc7789db
Mohammad Kasiri <mohammad.kasirey@gmail.com>
2022-07-27 16:08:16 +0430

    End Of 6 Episode

--- routes/web.php
+++ routes/web.php
@@ -14,8 +14,49 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
+    \App\Models\Article::query()->latest()->first()->getFirstMedia()->getPath();
+    \App\Models\Article::query()->latest()->first()->getFirstMedia()->getUrl();
+    \App\Models\Article::query()->latest()->first()->getFirstMedia();
+    \App\Models\Article::query()->latest()->first()->getMedia();
+
+    //Upload Single Media
     $article = ['title' => 'I am title' , 'body' => 'I am Article Body'];
     \App\Models\Article::create($article)
         ->addMedia(storage_path('demo/pic1.jpg'))
+        ->toMediaCollection();
+
+
+    //Upload Multiple Media
+    $article= \App\Models\Article::create($article);
+
+    $article->addMedia(storage_path('demo/pic1.jpg'))
+            ->toMediaCollection();
+
+    $article->addMedia(storage_path('demo/pic1.jpg'))
+        ->toMediaCollection();
+
+
+    //Upload Media From Request
+    $article= \App\Models\Article::create($article);
+
+    $article->addMediaFromRequest('image')  // 👉  name of input in request body
+            ->toMediaCollection();
+
+    //If You Want To Not Move, But Copy, The Original File You Can Call PreservingOriginal:
+    \App\Models\Article::create($article)
+        ->addMedia(storage_path('demo/pic1.jpg'))
+        ->preservingOriginal()
+        ->toMediaCollection();
+
+    //Override Specific Columns
+    \App\Models\Article::create($article)
+        ->addMedia(storage_path('demo/pic1.jpg'))
+        ->usingName()
+        ->usingFileName()
+        ->withCustomProperties([
+            'location' => 'Ireland',
+            'subject'  => 'A Nice Car'
+        ])
+        ->preservingOriginal()
         ->toMediaCollection();
 });
